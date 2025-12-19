7dcfb9da9a3e41dc69f1b8b5319f687bfacd2300
Pflame7 <plamenpanov22@gmail.com>
2025-12-19 18:26:13 +0200

    19.12.2025-коледноЗадание

--- ComponentTree/src/components/Main.jsx
+++ ComponentTree/src/components/Main.jsx
@@ -5,7 +5,6 @@ export default function Main(props) {
   return (
     <main className="main">
       <Sidebar />
-      {/* взимаме posts от App и ги даваме на Content */}
       <Content posts={props.posts} />
     </main>
   );
